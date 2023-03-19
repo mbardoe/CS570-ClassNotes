0c9a9edf56bfb489fa2374c421685035683b80f2
Matthew Bardoe <mbardoe@gmail.com>
2023-03-19 14:21:11 -0400

    Implemented landscape mode and some output to explain what is happening. Added README to the tools folder to explain how Renderer works.

--- tools/render.py
+++ tools/render.py
@@ -1,30 +1,32 @@
 import os
 from subprocess import call
 
+HOME_DIRECTORY = ".."
+
 
 # call(["ls", "-l"]
 
 class Renderer:
-    def __init__(self):
-        pass
+    def __init__(self, home_directory: str = ''):
+        self.home_directory = home_directory
 
     def render(self):
-        os.chdir("..")
+        os.chdir(self.home_directory)
         print(f"Current Top Directory: {os.getcwd()}")
         #######################
         # get list of all folders in this directory
         ######################
         my_directories = [f.path for f in os.scandir(os.getcwd()) if f.is_dir()]
-        pdf_direc = []
+        pdf_directory = []
         for directory in my_directories:
             if 'pdf' in [x.name for x in os.scandir(directory)]:
-                pdf_direc.append(directory)
+                pdf_directory.append(directory)
 
-        #print(pdf_direc)
+        # print(pdf_directory)
         ####################
         # Go through those directories and find all the .md files and then create the pdf
         ####################
-        for directory in pdf_direc:
+        for directory in pdf_directory:
             print(f"Looking in this directory for md to make into pdfs: \n {directory}")
             os.chdir(directory)
             md_files = [my_file.name for my_file in os.scandir(directory) if my_file.name[-3:] == ".md"]
@@ -35,32 +37,32 @@ class Renderer:
             for file in md_files:
                 self.process_file(directory, file)
 
-    def process_file(self, direc: str, filename:str):
-        os.chdir(direc)
+    def process_file(self, directory: str, filename: str):
+        os.chdir(directory)
         file1 = open(filename, "r")
-        Lines = file1.readlines()
-
-        count = 0
+        my_lines = file1.readlines()
         # Strips the newline character
-        commands = [line.strip()[10:].strip() for line in Lines if line.strip()[:10] == "[comment]:"]
+        commands = [line.strip()[10:].strip() for line in my_lines if line.strip()[:10] == "[comment]:"]
         print(f"In the file {filename} we found the commands: {commands}")
 
-        final_call=[]
+        final_call = []
         if 'render' in commands:
             if 'landscape' in commands:
-                final_call = ["pandoc", "-s", direc + "/" + filename,
+                final_call = ["pandoc", "-s", directory + "/" + filename,
                               '-V', 'geometry:landscape',
-                              "-o", direc + "/pdf/" + filename[:-3] + '.pdf']
+                              "-o", directory + "/pdf/" + filename[:-3] + '.pdf']
 
             else:
-                final_call=["pandoc", "-s", direc + "/" + filename, "-o", direc + "/pdf/" + filename[:-3] + '.pdf']
+                final_call = ["pandoc", "-s", directory + "/" + filename, "-o",
+                              directory + "/pdf/" + filename[:-3] + '.pdf']
 
-        if len(final_call)>0:
-            #print (final_call)
+        if len(final_call) > 0:
+            # print (final_call)
             call(final_call)
+
 
 if __name__ == '__main__':
     # Renderer.render()
-    r = Renderer()
-    #r.process_file("/Users/mbardoe/Documents/GitHub/CS570-ClassNotes/Homework_Handouts", 'HW_1.md')
+    r = Renderer(HOME_DIRECTORY)
+    # r.process_file("/Users/mbardoe/Documents/GitHub/CS570-ClassNotes/Homework_Handouts", 'HW_1.md')
     r.render()
